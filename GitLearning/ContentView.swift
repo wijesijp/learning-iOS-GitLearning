e6edc9884563661bee7e4b8f8eddcb8b181041d1
Janaka Wijesena <wijesena@gmail.com>
2025-05-05 17:13:44 +0530

    Added navigation link to ContentView and added DetailView

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                NavigationLink(destination: {
+                    DetailView()
+                }, label: {
+                    Text("Git Learning App")
+                })
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ GitLearning/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  GitLearning
+//
+//  Created by Janaka Wijesena on 2025-05-05.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("An app to learn GIT")
+    }
+}
+
+#Preview {
+    DetailView()
+}
