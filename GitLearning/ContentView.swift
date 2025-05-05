2c4b6286833efe4f948601b7a490bd4f7d535338
Janaka Wijesena <wijesena@gmail.com>
2025-05-05 17:02:19 +0530

    Initial Commit

---
+++ GitLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitLearning
+//
+//  Created by Janaka Wijesena on 2025-05-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Janaka Wijesena on 2025-05-05.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
